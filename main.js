609a5729857ba911a9bc5b0a5664d3743492ace1
Bonu93 <luca.morucci.kb@gmail.com>
2021-11-08 15:59:27 +0100

    complete

--- main.js
+++ main.js
@@ -145,9 +145,12 @@ A partire da un array di numeri, genera un secondo array con le potenze al quadr
 Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25]
 *************/
 
-
-
-
+//first array
+const numberList = [4, 9, 12, 34, 2, 3, 5, 11, 13];
+console.log(numberList);
+//square array
+const squareNumbers = numberList.map(number => number * number);
+console.log(squareNumbers);
 
 
 
